6911f121a78cca07f6e99dd6ddf03ddbf0df5480
miaha321 <miahaahaim89@gmail.com>
2024-11-16 15:35:51 +0200

    print clock for site

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function UpdateClock() {
-
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
 
-document.getElementById('clock').textContent = new Date()
+setInterval(UpdateClock, 1000);
