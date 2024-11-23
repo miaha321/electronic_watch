022964460f8c23d6ce5ca9e8de52633e62b775ca
miaha321 <miahaahaim89@gmail.com>
2024-11-23 16:35:47 +0200

    add UTCTIme with select country

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+const clockElement = document.getElementById('clock');
+const selectTimeZona = document.getElementById('timeZona');
+const local_timezona = document.getElementById('local_timezona')
+
+
 function updateClock() {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
-    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+    const timeZona = parseInt(selectTimeZona.value, 10);
+    //console.log(timeZona);
+
+    const hours = String(now.getUTCHours() + timeZona).padStart(2, "0");
+    const minutes = String(now.getUTCMinutes()).padStart(2, "0");;
+    const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+    local_timezona.textContent = Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
 setInterval(updateClock, 1000);
+selectTimeZona.addEventListener('change', updateClock);
+
+// const difrent_time_UTC = new Date().getTimezoneOffset() / 60;
+// const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZone;
+// document.getElementById('clock2').textContent = timeZona2;
